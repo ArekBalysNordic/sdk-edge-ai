90cf277ed77c17791fb048a0aa11432f62360efc
Adam Maciuga <adam.maciuga@nordicsemi.no>
2026-05-18 14:08:05 +0200

    Updated to wider-acent 'Okay Nordic' model
    The wakeword should now be better recognized
    
    Signed-off-by: Adam Maciuga <adam.maciuga@nordicsemi.no>

--- applications/matter_switch/src/models/wakeword_Nordic_Light/nrf_edgeai_generated/nrf_edgeai_user_types.h
+++ applications/matter_switch/src/models/wakeword_Nordic_Light/nrf_edgeai_generated/nrf_edgeai_user_types.h
@@ -1,4 +1,9 @@
-/* 2026-01-29T20:26:36.116161 */
+/* 2026-04-22T08:18:23.060530 */
+
+/*
+* Copyright (c) 2026 Nordic Semiconductor ASA
+* SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+*/
 
 #ifndef _NRF_EDGEAI_USER_TYPES_H_
 #define _NRF_EDGEAI_USER_TYPES_H_
@@ -13,8 +18,8 @@ extern "C"
 typedef int16_t    nrf_user_input_t;
 typedef int32_t    nrf_user_feature_t;
 typedef flt32_t    nrf_user_output_t;
-typedef uint16_t   nrf_user_coeff_t;
-typedef int16_t    nrf_user_weight_t;
+typedef uint8_t   nrf_user_coeff_t;
+typedef int8_t    nrf_user_weight_t;
 typedef nrf_user_coeff_t nrf_user_neuron_t;
 
 #ifdef   __cplusplus
